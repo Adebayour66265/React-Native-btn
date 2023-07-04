b4b1b809ca663d3ad296e59a512a689ec2a214ba
Adebayour66265 <adebayour66265@gmail.com>
2023-07-04 11:14:07 +0100

    first commit

--- App.js
+++ App.js
@@ -1,144 +1,117 @@
-import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import {
-  Button,
-  Pressable,
-  StyleSheet,
-  Switch,
-  Text,
-  useWindowDimensions,
-  View,
-} from "react-native";
-import {
-  BottomSheetModal,
-  BottomSheetModalProvider,
-} from "@gorhom/bottom-sheet";
-import { useRef, useState } from "react";
+import React, { useState } from "react";
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-native-paper";
+import BottomSheet from "./bottom-sheet/BottomSheet";
 import { AntDesign } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
 import Icons, { icons } from './src/components/Icons'
 
-
 export default function App() {
-
-  const { width } = useWindowDimensions();
-  const [theme, setTheme] = useState("dim");
+  const [show, setShow] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
-  const bottomSheetModalRef = useRef(null);
-
-  const snapPoints = ["52%", "75%"];
-
-  function handlePresentModal() {
-    bottomSheetModalRef.current?.present();
-    setTimeout(() => {
-      setIsOpen(true);
-    }, 100);
-  }
-
-
-
   return (
-    <BottomSheetModalProvider>
+    <Provider>
       <View
         style={[
           styles.container,
           { backgroundColor: isOpen ? "gray" : "white" },
         ]}
       >
-        <Text style={[styles.title, { marginBottom: 20 }]} onPress={handlePresentModal} >Your Shortcuts</Text>
-        <StatusBar style="auto" />
-        <BottomSheetModal
-          ref={bottomSheetModalRef}
-          index={1}
-          snapPoints={snapPoints}
-          enablePanDownToClose={true}
-          backgroundStyle={{ borderRadius: 10 }}
-          onDismiss={() => setIsOpen(true)}
+        <Text style={[styles.titles, { marginBottom: 20 }]} onPress={() => setShow(true)} >Your Shortcuts</Text>
+        <BottomSheet
+          show={show}
+          onDismiss={() => {
+            setShow(false);
+          }}
+          enableBackdropDismiss
         >
-          <View style={styles.contentContainer}>
-            <Text style={[styles.titles, { marginBottom: 20 }]}>Your Shortcuts </Text>
-            <View style={styles.shortcut}>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Icons icon={icons.Feather} name="phone-call" color={'white'} />
+          <ScrollView contentContainerStyle={{ padding: 16 }}>
+            <View style={styles.contentContainer}>
+              <Text style={[styles.titles, { marginBottom: 20 }]
+              }
+              >Your Shortcuts</Text>
+              <View style={styles.shortcut}>
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Icons icon={icons.Feather} name="phone-call" color={'white'} />
+                  </View>
+                  <Text style={styles.title}>Contact Us</Text>
                 </View>
-                <Text style={styles.title}>Contact Us</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Icons icon={icons.Entypo} name="dots-three-vertical" />
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Icons icon={icons.Entypo} name="dots-three-vertical" />
+                  </View>
+                  <Text style={styles.title}>Food & Grocery</Text>
                 </View>
-                <Text style={styles.title}>Food & Grocery</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Icons icon={icons.Feather} name="search" />
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Icons icon={icons.Feather} name="search" />
+                  </View>
+                  <Text style={styles.title}>Explore B-Prime</Text>
                 </View>
-                <Text style={styles.title}>Explore B-Prime</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Icons icon={icons.Entypo} name="dots-three-vertical" />
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Icons icon={icons.Entypo} name="dots-three-vertical" />
+                  </View>
+                  <Text style={styles.title}>Bloozon Pharmacy</Text>
                 </View>
-                <Text style={styles.title}>Bloozon Pharmacy</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Text style={styles.productName}>Top Brands</Text>
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Text style={styles.productName}>Top Brands</Text>
+                  </View>
+                  <Text style={styles.title}>Top Brands</Text>
                 </View>
-                <Text style={styles.title}>Top Brands</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Icons icon={icons.Entypo} name="home" />
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Icons icon={icons.Entypo} name="home" />
+                  </View>
+                  <Text style={styles.title}>Luxury Store</Text>
                 </View>
-                <Text style={styles.title}>Luxury Store</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Icons icon={icons.Entypo} name="box" />
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Icons icon={icons.Entypo} name="box" />
+                  </View>
+                  <Text style={styles.title}>Bloozon Product</Text>
                 </View>
-                <Text style={styles.title}>Bloozon Product</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Text style={styles.productName}>Fan Shop</Text>
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Text style={styles.productName}>Fan Shop</Text>
+                  </View>
+                  <Text style={styles.title}>Fan Shop</Text>
                 </View>
-                <Text style={styles.title}>Fan Shop</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Icons icon={icons.Entypo} name="dots-three-vertical" />
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Icons icon={icons.Entypo} name="dots-three-vertical" />
+                  </View>
+                  <Text style={styles.title}>Todays Deals</Text>
                 </View>
-                <Text style={styles.title}>Todays Deals</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Icons icon={icons.Feather} name="help-circle" />
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Icons icon={icons.Feather} name="help-circle" />
+                  </View>
+                  <Text style={styles.title}>Help Directive</Text>
                 </View>
-                <Text style={styles.title}>Help Directive</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Text style={styles.productName}>Bloozon Business</Text>
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Text style={styles.productName}>Bloozon Business</Text>
+                  </View>
+                  <Text style={styles.title}>Join Bloozon Business</Text>
                 </View>
-                <Text style={styles.title}>Join Bloozon Business</Text>
-              </View>
-              <View style={styles.box} >
-                <View style={styles.card}>
-                  <Icons icon={icons.Entypo} name="shopping-bag" />
+                <View style={styles.box} >
+                  <View style={styles.card}>
+                    <Icons icon={icons.Entypo} name="shopping-bag" />
+                  </View>
+                  <Text style={styles.title}>Used Item</Text>
                 </View>
-                <Text style={styles.title}>Used Item</Text>
               </View>
             </View>
-
-
-
-          </View>
-        </BottomSheetModal>
+          </ScrollView>
+        </BottomSheet>
+        <StatusBar style="auto" />
       </View>
-    </BottomSheetModalProvider>
+    </Provider>
   );
 }
 
@@ -173,7 +146,7 @@ const styles = StyleSheet.create({
   card: {
     borderRadius: 10,
     backgroundColor: '#40E0D0',
-    padding: 10
+    padding: 12
   },
   titles: {
     fontWeight: "900",
